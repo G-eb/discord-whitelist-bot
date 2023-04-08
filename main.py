a85a33cc55de5569900bafd1caa65067e62f0edb
Geb <83452307+G-eb@users.noreply.github.com>
2023-04-09 00:23:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import discord
 from discord.ext import commands
 import json
 
-client = commands.Bot(help_command=None)
+intents = discord.Intents.default()
+intents.members = True
+client = commands.Bot(help_command=None, intents=intents)
 
 # full credits of this code goes to Geb#1337
 
@@ -23,13 +25,13 @@ async def on_member_join(member):
     whitelist = open("whitelist.txt", "r")
     whitelisted = whitelist.read() 
     if(str(member.id) in whitelisted):
-        role = discord.utils.get(member.guild.roles, id=roleid)
+        role = discord.utils.get(member.guild.roles, id=int(roleid))
         await member.add_roles(role)
         await member.send(f"You have been whitelisted in `{member.guild.name}`!")
     else:
-        await member.send(f"You are not currently whitelisted in `{member.guild.name}`!\nA whitelist request has been sent, please try to join the server again in a few days.`")
+        await member.send(f"You are not currently whitelisted in `{member.guild.name}`!\nA whitelist request has been sent, please try to join the server again in a few days.")
         await member.ban(reason="Not whitelisted!")
-        channel = await client.get_channel(requestschannel)
+        channel = client.get_channel(int(requestschannel))
         await channel.send(f"{member.mention} has requested to join the server.\n\nRun `/accept id:{member.id}` to accept, or `/deny id:{member.id}` to deny.")
     whitelist.close()
 
@@ -38,16 +40,18 @@ async def ping(ctx):
     await ctx.respond(f":ping_pong: {round(client.latency * 1000)}ms")
 
 @client.slash_command(guild_ids=[guildid])
-async def accept(ctx, id: int):
+async def accept(ctx, id: str):
     if ctx.author.guild_permissions.administrator:
         f = open("whitelist.txt", "a")
-        f.write(str(id)  + "\n")
+        f.write(id + "\n")
+        user = await client.fetch_user(id)
+        await ctx.guild.unban(user=user, reason="Whitelisted!")
         await ctx.respond(f"Accepted <@{id}> [`{id}`]!")
     else:
         await ctx.respond("You don't have permission to use this command!", ephemeral=True)
 
 @client.slash_command(guild_ids=[guildid])
-async def deny(ctx, id: int):
+async def deny(ctx, id: str):
     if ctx.author.guild_permissions.administrator:
         await ctx.respond(f"Denied <@{id}> [`{id}`]!")
     else:
